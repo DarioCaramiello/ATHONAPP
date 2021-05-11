9431c20cf6cef163e6d1750224f5a283e8389103
VincenzoRusso674 <vincenzo.russo007@studenti.uniparthenope.it>
2021-05-11 22:34:18 +0200

    Dario gay

--- js/main.js
+++ js/main.js
@@ -2,11 +2,8 @@ let name;
 
 function set_name() {
     let id = document.getElementById("utent_id");
-
     console.log("Hi, by set_name()");
-
     name = id.value;
-
     console.log("global name : "+name)
     location.href="page2.html"
 }
@@ -14,5 +11,6 @@ function set_name() {
 function PopUpRegole(){
     let id=document.getElementById("regole")
     console.log("Le regole")
+    console.log("Dario gay")
 }
 
