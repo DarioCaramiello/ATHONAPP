0f9389b1b15ba38f098a2aaf0b4f29fc0ae04097
dario <dario.caramiello001@studenti.unitpartenope.it>
2021-05-12 12:24:03 +0200

    fix and update : index.html / style.css Non modificare o eliminare codice gia scritto, aggiungere solo con linee di commenti.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,8 @@
 
-let name;
 
 function start_game() {
     console.log("Hi, by start_game()");
     location.href="page2.html"
-}
-
-function PopUpRegole(){
-    let id=document.getElementById("regole")
-    console.log("Le regole")
-    console.log("Dario gay")
 }
 
 
