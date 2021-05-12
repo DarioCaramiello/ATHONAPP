521e0da6db73b0955e3d500adf254b9685387570
dario <dario.caramiello001@studenti.unitpartenope.it>
2021-05-12 17:10:34 +0200

    update : main.js / index.html / style.css

--- js/main.js
+++ js/main.js
@@ -6,20 +6,10 @@ function start_game() {
 }
 
 function change_color_on() {
-    document.getElementById("btn_id").style.backgroundColor = "white"
+    document.getElementById("btn_id").style.color = "white"
 }
 
 function change_color_off() {
-    document.getElementById("btn_id").style.backgroundColor = "rgba(255,255,255,0.1)"
-}
-
-function change_size_on() {
-    document.getElementById("cite_intro").style.fontSize = "25pt"
-    document.getElementById("cite_intro").style.color = "green"
-}
-
-function change_size_off() {
-    document.getElementById("cite_intro").style.fontSize = "17pt"
-    document.getElementById("cite_intro").style.color = "black"
+    document.getElementById("btn_id").style.color = "black"
 }
 
