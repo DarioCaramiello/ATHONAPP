5d698f97fd26854ebaa2819f92055e20e73d5d76
dario <dario.caramiello001@studenti.unitpartenope.it>
2021-05-12 22:37:24 +0200

    update insert nick - insert variable 'point_rank' in main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 let playerName;
+let point_rank;
 
 function start_game() {
     console.log("Hi, by start_game()");
     playerName=  document.getElementById("nickname").value; //Salva nome giocatore
     console.log(playerName);
     location.href="page2.html"
+    point_rank = 0;
 }
 
 function change_color_on() {
@@ -19,7 +21,7 @@ function change_color_off() {
 
 //Funzioni per il form
 function closeForm(){
-    let x = document.getElementById("myForm").style.display = "none"
+    let x = document.getElementById("myForm").style.display = "none";
 }
 
 function openForm() {
