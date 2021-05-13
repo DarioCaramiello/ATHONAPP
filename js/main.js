cf254aeee63cf28cbefc891446b3ba7bb39ae27d
dario <dario.caramiello001@studenti.unitpartenope.it>
2021-05-13 18:13:17 +0200

    form passato alla seconda pagina, idem funzioni in ' main2.js '. idem style in 'style2.css'

--- js/main.js
+++ js/main.js
@@ -1,18 +1,10 @@
-let playerName;
-let point_rank;
-
 function start_game() {
-    console.log("Hi, by start_game()");
-    playerName=  document.getElementById("nickname").value; //Salva nome giocatore
-    console.log(playerName);
     location.href="page2.html"
-    point_rank = 0;
 }
 
 
 /* funzione del cambio di colore migliorata */
 function change_color() {
-    console.log("hi, by change_color()");
     let obj = document.getElementById("btn_id")
     if( obj.style.color === "white") {
         obj.style.color = "black"
@@ -21,14 +13,27 @@ function change_color() {
     }
 }
 
-/*  Funzioni per il cambio displey ( show - hidden ).
+function for_cite_on() {
+    let cite = document.getElementById("cite_intro").style
+    cite.fontSize = "26pt"
+    cite.color = "white"
+}
+
+function for_cite_off() {
+    let cite = document.getElementById("cite_intro").style
+    cite.fontSize = "20pt"
+    cite.color = "black"
+}
+
+/*
+
+  Funzioni per il cambio displey ( show - hidden ).
     Note : ho provato ad ottimizare la funzione come in 'change color ' solo che non puo essere
     ottmizzata perche altrimenti il senso logico verrebbe corrotto
-*/
+
 function change_display_on () {
-    console.log("hi , by change_display_on()")
     document.getElementById("myForm").style.display = "block"
     document.getElementById("span_id").style.display = "none"
 }
 
-
+ */
--- js/main2.js
+++ js/main2.js
@@ -1,32 +1,14 @@
 
+function change_border_on() {
+    let x = document.getElementById("login_id")
+    x.style.borderRadius = "40px"
+    x.style.backgroundColor = "black"
+    x.style.color = "white"
+}
 
-let canvas = document.getElementById('myCanvas')
-canvas.width = 600
-canvas.height = 600
-
-let ctx = canvas.getContext('2d')
-
-/* 1o traingolo in alto a sinistra */
-ctx.beginPath()
-ctx.moveTo(300,100)
-ctx.lineTo(300,300)
-ctx.lineTo(200,300)
-ctx.closePath()
-ctx.stroke()
-
-/* 2o triangolo in alto a destra */
-ctx.beginPath()
-ctx.moveTo(300,100)
-ctx.lineTo(400,300)
-ctx.lineTo(300,300)
-ctx.stroke()
-
-ctx.beginPath()
-ctx.moveTo(300,300)
-ctx.lineTo(300,500)
-ctx.stroke()
-
-
-
-
-
+function change_border_off() {
+    let x = document.getElementById("login_id")
+    x.style.borderRadius = "0"
+    x.style.backgroundColor = "#ffffff"
+    x.style.color = "black"
+}
