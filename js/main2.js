915184f512aa4274c6ba2470033f3deae0f4afca
dario <dario.caramiello001@studenti.unitpartenope.it>
2021-05-13 19:08:27 +0200

    form passato alla seconda pagina, idem funzioni in ' main2.js '. idem style in 'style2.css'

--- js/main2.js
+++ js/main2.js
@@ -1,14 +1,30 @@
 
+let point_rank
+let player_name
+
+// function for dynamic buttom login
 function change_border_on() {
     let x = document.getElementById("login_id")
     x.style.borderRadius = "40px"
     x.style.backgroundColor = "black"
     x.style.color = "white"
 }
-
 function change_border_off() {
     let x = document.getElementById("login_id")
     x.style.borderRadius = "0"
-    x.style.backgroundColor = "#ffffff"
+    x.style.backgroundColor = "#80ed99"
     x.style.color = "black"
 }
+
+
+function start () {
+
+    player_name = document.getElementById("nickname").value
+    point_rank = 0
+    console.log("player name : "+player_name)
+    console.log("point rank : "+point_rank)
+
+    document.getElementById("container_start").style.display = "none"
+
+}
+
