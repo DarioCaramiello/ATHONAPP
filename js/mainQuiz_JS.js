ee3738a3baa2487b31d65842b097273a43ae4849
dario <dario.caramiello001@studenti.unitpartenope.it>
2021-05-17 16:59:37 +0200

    new file

--- js/mainQuiz_JS.js
+++ js/mainQuiz_JS.js
@@ -1,35 +1,15 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
 
-/*https://www.html.it/pag/15203/eventi-legati-al-trascinamento-del-mouse/ */
-let a = document.getElementById("id_function")
-let b = document.getElementById("id_while")
-let c = document.getElementById("id_def")
-let d = document.getElementById("id_struct")
-let e = document.getElementById("id_return")
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
 
-a.ondragstart = function(e) {
-    e.dataTransfer.setData("text", e.target.id);
-}
-b.ondragstart = function(e) {
-    e.dataTransfer.setData("text", e.target.id);
-}
-c.ondragstart = function(e) {
-    e.dataTransfer.setData("text", e.target.id);
-}
-d.ondragstart = function(e) {
-    e.dataTransfer.setData("text", e.target.id);
-}
-e.ondragstart = function(e) {
-    e.dataTransfer.setData("text", e.target.id);
+function drop (ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
 }
 
 
-var divTarget = document.getElementById("cella_utent1");
-divTarget.ondragover = function(e) {
-    e.target.className = "cella";
-    e.preventDefault();
-}
-
-divTarget.ondrop = function(e) {
-    var data = e.dataTransfer.getData("text");
-    e.target.appendChild(document.getElementById("id_function"));
-}
