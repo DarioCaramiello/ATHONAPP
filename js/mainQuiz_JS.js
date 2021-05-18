780d09858b025e94d05cbe64f445badf8d77821e
dario <dario.caramiello001@studenti.unitpartenope.it>
2021-05-18 18:42:51 +0200

    new file

--- js/mainQuiz_JS.js
+++ js/mainQuiz_JS.js
@@ -13,9 +13,28 @@ function drop (ev) {
 }
 
 function verifica_quiz() {
+    let count_ris = 0;
+    let risp_row1 = "def"
+    let risp_row2 = "gamma"
+
     let x = document.getElementById("cella_utent1").innerText
     let y = document.getElementById("cella_utent2").innerText
     let z = document.getElementById("cella_utent3").innerText
+
+    if ( x===risp_row1 ) {
+        count_ris += 1
+    }
+    if ( y === risp_row2 ) {
+        count_ris += 1
+    }
+
+    if(count_ris === 2 ){
+        document.getElementById("risp_corretta").style.display = "block"
+    } else {
+        document.getElementById("risp_errata").style.display = "block"
+    }
+
+    /* da implementare la funzione per aggiungere i punti ( serve conoscenza lato server ) */
 
     console.log(x)
     console.log(y)
