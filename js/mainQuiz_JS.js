c65e232e6975158f55b7223d01f8a88614cf4cbf
Simone-Lanza <simone.lanza001@studenti.uniparthenope.it>
2021-05-23 12:49:44 +0200

    pagina quiz js

--- js/mainQuiz_JS.js
+++ js/mainQuiz_JS.js
@@ -47,19 +47,26 @@ function verifica_quiz() {
 }
 
 //timer
-
-$('.go').on('click', function() {
-    $('.clock').addClass('count');
-});
-
-let timer = 5;
+var time_out = "Time out";
+/*per cambiare font size alla scritta Time out*/
+var result_time_out = time_out.fontsize(6);
+let timer = 30;
 // Update the count down every 1 second
 let x = setInterval(function() {
     timer -= 1
-    document.getElementById("timer").innerHTML = timer;
+    document.getElementById("timer_value").innerText = timer;
+    /*per spostare i numeri al centro*/
+    if(timer < 10) {
+        document.getElementById("timer_value").style.marginLeft = "15px";
+    }
     if (timer < 0) {
         clearInterval(x);
-        document.getElementById("timer").innerHTML = "Time-out";
+        document.getElementById("timer_value").style.marginLeft = "-40px";
+        document.getElementById("timer_value").innerHTML = result_time_out;
+        document.getElementById("timer_value").style.color = "#DDD92A";
+        document.getElementById("timer_value").style.textShadow = "2px 2px 1px #ff0000,-2px -2px 1px #F56416, 2px -2px 1px #E28413, -2px 2px 1px #EA1744";
+        /*per nascondere l'animazione del timer*/
+        $('.circle_animation').hide();
         verifica_quiz();
     }
     let confirm_button_id = document.getElementById("input_button")
