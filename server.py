12ea2772e6afd8278ec7e863d5839481e0b062b2
DarioCaramiello <83814756+DarioCaramiello@users.noreply.github.com>
2021-06-01 11:11:37 +0200

    New Server with MongoDB

--- server.py
+++ server.py
@@ -1,96 +1,57 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import random
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app, resource={r"/api/*": {"origins": "*"}})
 
-# dizionario che memorizza i login
-utenti = {}
-tot_user = len(utenti)
-# array globale di supporto alla route 'return-index'
-index_mem = []
-premi = [
-        ["PS Store Credit" , "Dogecoing", "Amazon Credit"],
-        ["Google Credit", "Prize2" , "Prize3"],
-        ["Day3prie", "PRize2", "prize3"],
-        ["Day4prie", "PRize2", "prize3"],
-        ["Day5prie", "PRize2", "prize3"],
-        ["Day6prie", "PRize2", "prize3"],
-        ["Day7prie", "PRize2", "prize3"],
-    ]
-#dizionario dei quiz
-quiz ={
-    "CSS": "false",
-    "JS": "false",
-    "HTML": "false",
-    "LOGIC": "false",
-}
-
-@app.route('/api/update-quiz', methods=['POST'])
-def quiz_done():
-    quiz_aggiornato = request.form.get('quiz','non definito')
-    stato = request.form.get('stato', 'false')
-    quiz[quiz_aggiornato]= stato
-    return str('quiz aggiornato')
-
-
-@app.route('/api/root', methods=['GET'])
-def give_quiz():
-    return jsonify(quiz)
-
+client = MongoClient("mongodb://localhost:27017/")
+db = client["database"]
+user_ = db["user"]
 
 
 @app.route('/')
 def root():
-    return jsonify(utenti)
+    risultato = {0: "SERVER"}
+    return jsonify(risultato)
 
-@app.route('/api/server-login', methods=['POST'])
-def name_user():
-    nickname = str(request.form.get('nickname'))
-    password = int(request.form.get('password'))
-    point = 0
 
-    # if utente in database:
-        # if password == utente.password:
-            # return str("accesso consentito")
-        # else:
-            # return str("accesso negato")
-    # else :
-        # inserisci nel database
+@app.route('/show-users')
+def show():
+    x = user_.find({}, {"_id": 0})
+    list_user = []
+    for item in x:
+        list_user.append(item)
+    result = {"lista utenti": list_user}
+
+    return jsonify(result)
 
 
 @app.route('/api/server-login', methods=['POST'])
-def name_user():
-    index = random.randint(1, 10)
-    index_mem.append(index)
-    for key in utenti:
-        if utenti[key]['name'] == request.form.get('nickname', 'non definito'):
-            return jsonify(utenti[index_mem[0]])
-    utenti[index] = {'name': request.form.get('nickname', 'non definito'), 'point': 0}
-    return jsonify(utenti[index_mem[0]], premi)
+def login():
+    x = user_.find_one({"nickname": str(request.form.get('nickname'))})
+    if x != None:
+        if x['password'] == request.form.get('password'):
+            out = {"access": 1,"name": x['nickname'], "point": x['point'] }
+            return jsonify(out)
+        else:
+            out = {"access": 0}
+            return jsonify(out)
+    else:
+        id_user = {
+            "nickname": str(request.form.get('nickname')),
+            "password": request.form.get('password'),
+            "point": 0,
+            "game_": 0
+        }
+        user_.insert_one(id_user)
+        out = { "access": "create" }
+        return jsonify(out)
 
-# dopo aver richiamato il login , ogni quiz quando si carica richiama il server che restituisce l'index
-# associato al dizionario per poi fare una request per aggiornamento di punti per quell'index
-@app.route('/api/return-index', methods=['POST', 'GET'])
-def get_index():
-    var = index_mem[0]
-    index_mem.remove(index_mem[0])
-    return str(var)
 
-
-# il client invia al server l'index piu il punteggio aggiuntivo
-# il server aggiorna la classifica
-@app.route('/api/update-point', methods=['POST', 'GET'])
-def update_rank():
-    point_add = request.form.get('point')
-    index = request.form.get('index')
-    index_int = int(index)
-    point_add_int = int(point_add)
-
-    point_user = utenti[index_int]['point']
-    point_user_int = int(point_user)
-    sum_ = point_user_int + point_add_int
-    sum_int = int(sum_)
-    utenti[index_int]['point'] = sum_int
-    return str("update")
+# API per eliminare la collezione
+@app.route('/clear-db')
+def clear():
+    user_.drop()
+    out = {"action": 1}
+    return jsonify(out)
