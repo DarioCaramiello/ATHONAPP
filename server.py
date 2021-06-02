c446ad356c3a99bad6658592e5d8cd0d2adc4f58
VincenzoRusso674 <83902395+VincenzoRusso674@users.noreply.github.com>
2021-06-02 22:12:07 +0200

    Premi e quiz
    aggiunto premi e quiz nel server

--- server.py
+++ server.py
@@ -3,12 +3,22 @@ from flask_cors import CORS
 from pymongo import MongoClient
 
 app = Flask(__name__)
-CORS(app, resource={r"/api/*": {"origins": "*"}})
+CORS(app, resource={r"/api/": {"origins": ""}})
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["database"]
 user_ = db["user"]
+name_corrent_user = [" "]
 
+premi = [
+        ["PS Store Credit" , "Dogecoing", "Amazon Credit"],
+        ["Google Credit", "Prize2" , "Prize3"],
+        ["Day3prie", "PRize2", "prize3"],
+        ["Day4prie", "PRize2", "prize3"],
+        ["Day5prie", "PRize2", "prize3"],
+        ["Day6prie", "PRize2", "prize3"],
+        ["Day7prie", "PRize2", "prize3"],
+    ]
 
 @app.route('/')
 def root():
@@ -27,26 +37,34 @@ def show():
     return jsonify(result)
 
 
+@app.route('/api/show-prizes', methods=['GET'])
+def show_prizes():
+    return jsonify(premi)
+
+
 @app.route('/api/server-login', methods=['POST'])
 def login():
     x = user_.find_one({"nickname": str(request.form.get('nickname'))})
     if x != None:
         if x['password'] == request.form.get('password'):
-            out = {"access": 1,"name": x['nickname'], "point": x['point'] }
-            return jsonify(out)
+            out = {"access": 1, "name": x['nickname'], "point": x['point']}
+            name_corrent_user[0] = str(request.form.get('nickname'))
         else:
             out = {"access": 0}
-            return jsonify(out)
     else:
         id_user = {
             "nickname": str(request.form.get('nickname')),
             "password": request.form.get('password'),
             "point": 0,
-            "game_": 0
+            "CSS": 0,
+            "LOGIC": 0,
+            "JS": 0,
+            "HTML": 0
         }
         user_.insert_one(id_user)
-        out = { "access": "create" }
-        return jsonify(out)
+        out = {"access": "create", "name": id_user["nickname"], "point": id_user["point"]}
+        name_corrent_user[0] = id_user["nickname"]
+    return jsonify(out)
 
 
 # API per eliminare la collezione
@@ -54,4 +72,44 @@ def login():
 def clear():
     user_.drop()
     out = {"action": 1}
+    return jsonify(out)
+
+
+@app.route('/api/get-nickname', methods=['POST'])
+def get_nickname():
+    out = {"nickname": str(name_corrent_user[0])}
+    return jsonify(out)
+
+
+@app.route('/api/update-point', methods=['POST'])
+def add_point():
+    x = user_.find_one({"nickname": str(request.form.get('name'))})
+    point_Now = x['point']
+    point_Add = request.form.get('point')
+    sum = point_Now + int(point_Add)
+    myquery = {"nickname": request.form.get('name')}
+    newValue = {"$set": {"point": sum}}
+    user_.update_one(myquery, newValue)
+    return "update point"
+
+
+@app.route('/api/update-quiz', methods=['POST'])
+def quiz_done():
+
+    x = user_.find_one({"nickname": str(request.form.get('name'))})
+    quiz_aggiornato = request.form.get('quiz','non definito')
+    stato = request.form.get('stato', 0)
+    myquery = {"nickname": request.form.get('name')}
+    newValue = {"$set": {quiz_aggiornato: stato}}
+    user_.update_one(myquery, newValue)
+
+    return str('quiz aggiornato')
+
+
+@app.route('/api/get-quiz', methods=['POST'])
+def get_quiz():
+
+    x = user_.find_one({"nickname": str(request.form.get('name'))})
+    out = {"CSS": x['CSS'], "LOGIC": x['LOGIC'], "JS": x['JS'], "HTML": x['HTML']}
+
     return jsonify(out)
