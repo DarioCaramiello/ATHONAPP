8e378ee2b6f2d5b8bacc2fd9376ff0949574dda0
dario <dario.caramiello001@studenti.unitpartenope.it>
2021-06-09 16:14:25 +0200

    update function server quiz HTML

--- js/mainQuiz_JS.js
+++ js/mainQuiz_JS.js
@@ -184,44 +184,51 @@ function return_main_page() {
 
 function timerStart(){
     var time_out = "Time out";
+    /* variabile bool per controllare se i numeri < 10 devono essere spostati*/
+    var check=0
     /*per cambiare font size alla scritta Time out*/
     var result_time_out = time_out.fontsize(6);
-    let timer = 30;
+    let timer = 60;
 // Update the count down every 1 second
     let x = setInterval(function() {
         timer -= 1
         document.getElementById("timer_value").innerText = timer;
         /*per spostare i numeri al centro*/
         if(timer < 10) {
-            document.getElementById("timer_value").style.marginLeft = "33px";
+            document.getElementById("timer_value").style.marginLeft = "25px";
+            check = 1
         }
         if (timer <= 0) {
             clearInterval(x);
             /*per nascondere l'animazione del timer*/
             $('.circle_animation').hide();
-
-     ;
+            $("#risp_errata").show()
+            $("#risp_corretta").hide()
+            $("#game_").hide()
+            $("#timer").hide()
+            $("#confirm_button").hide()
+            $("#id_question_quiz").hide()
+            $("#reset_button").show()
+            $('#riassunto_game').show()
             document.getElementById("timer_value").innerHTML = result_time_out;
-            document.getElementById("timer_value").style.color = "#DDD92A";
-
+            if(check===1){
+                check = 0
+                document.getElementById("timer_value").style.marginLeft = "10px";
+            }
         }
-
         /*quando clicca su conferma ed il tempo non è ancora finito, si blocca il tempo */
         let confirmButton = document.getElementById("input_button")
         confirmButton.addEventListener("click", () => {
             clearInterval(x)
             $('.circle_animation').hide();
-
-            document.getElementById("timer_value").style.color = "#DDD92A";
-
             document.getElementById("timer_value").innerHTML = result_time_out;
             /* document.getElementById("confirm_btn").style.display = "none" */
+            timer = 60
         })
-
         let nextButton = document.getElementById("reset_button")
         nextButton.addEventListener("click", () => {
             clearInterval(x)
-            document.getElementById("timer_value").innerHTML = 30
+            document.getElementById("timer_value").innerHTML = 60
             /* document.getElementById("confirm_btn").style.display = "none" */
         } )
     }, 1000);
